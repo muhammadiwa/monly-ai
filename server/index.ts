1a91770c9c1a7c9dff86611759d1781b833f496e
muhammadiwa <muhammad.iwa94@gmail.com>
2025-08-03 00:56:02 +0700

    feat reminder notif

--- server/index.ts
+++ server/index.ts
@@ -65,10 +65,9 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
+  // Use port from environment variable, fallback to 5000 if not set
   // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = parseInt(process.env.PORT || '5000');
   server.listen({
     port,
     host: "0.0.0.0",
